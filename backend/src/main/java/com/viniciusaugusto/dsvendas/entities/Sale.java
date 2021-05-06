f6615c3a45764e5a1ba8ddf46e60c94c71b4e744
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-05-05 22:44:17 -0300

    Pagination

--- backend/src/main/java/com/viniciusaugusto/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/viniciusaugusto/dsvendas/entities/Sale.java
@@ -1,8 +1,17 @@
 package com.viniciusaugusto.dsvendas.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
---
+++ backend/src/main/java/com/viniciusaugusto/dsvendas/services/SaleService.java
@@ -0,0 +1,31 @@
+package com.viniciusaugusto.dsvendas.services;
+
+import com.viniciusaugusto.dsvendas.dto.SaleDTO;
+import com.viniciusaugusto.dsvendas.entities.Sale;
+import com.viniciusaugusto.dsvendas.repositories.SaleRepository;
+import com.viniciusaugusto.dsvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SaleDTO> findAll(Pageable pageable) {
+        sellerRepository.findAll();
+        Page<Sale> result = repository.findAll(pageable);
+        return result.map(SaleDTO::new);
+    }
+}
